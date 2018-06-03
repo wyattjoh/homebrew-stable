a5dfabd7ac8f129d0d2fc55cb9e852f7ec8b95c5
goreleaserbot <goreleaser@carlosbecker.com>
2018-06-03 12:41:56 -0600

    Brew formula update for cloudflare-ddns version v1.0.4

--- Formula/cloudflare-ddns.rb
+++ Formula/cloudflare-ddns.rb
@@ -1,9 +1,9 @@
 class CloudflareDdns < Formula
   desc "Cloudflare Dynamic DNS Updater"
   homepage "https://github.com/wyattjoh/cloudflare-ddns"
-  url "https://github.com///releases/download/v1.0.2/cloudflare-ddns_v1.0.2_darwin_amd64.tar.gz"
-  version "1.0.2"
-  sha256 "67c2b250d461509b73c4da2957b601f8d24e913c7a6909ac8e750b12a265f5e8"
+  url "https://github.com/wyattjoh/cloudflare-ddns/releases/download/v1.0.4/cloudflare-ddns_v1.0.4_darwin_amd64.tar.gz"
+  version "1.0.4"
+  sha256 "ad4d9202c6692882d862bfc70a3f737fd12e264eec902787dd3159944ba9056d"
 
   def install
     bin.install "cloudflare-ddns"
