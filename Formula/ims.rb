e3625c6a928f3465cd83a7df9432b262158ad6d9
goreleaserbot <goreleaser@carlosbecker.com>
2020-07-20 20:47:18 +0000

    Brew formula update for ims version v1.3.6

--- Formula/ims.rb
+++ Formula/ims.rb
@@ -2,21 +2,21 @@
 class Ims < Formula
   desc "image manipulation service, written in Go"
   homepage "https://github.com/wyattjoh/ims"
-  version "1.3.5"
+  version "1.3.6"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/wyattjoh/ims/releases/download/v1.3.5/ims_v1.3.5_darwin_amd64.tar.gz"
-    sha256 "1e211c97dec63fb9fcf6e68200dd463c02cc18102d60a972bedfc73795dc8fb0"
+    url "https://github.com/wyattjoh/ims/releases/download/v1.3.6/ims_v1.3.6_darwin_amd64.tar.gz"
+    sha256 "66488f9a004fc9cf1bacf0f19eee1b5b7ef0da643207495352d0f2033bec5169"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/wyattjoh/ims/releases/download/v1.3.5/ims_v1.3.5_linux_amd64.tar.gz"
-      sha256 "015c329ad9967ff4a6d7e19ddf29ec1a6c158d23ea7c35854b420934622bce44"
+      url "https://github.com/wyattjoh/ims/releases/download/v1.3.6/ims_v1.3.6_linux_amd64.tar.gz"
+      sha256 "bf8b48b9fcafa9469e12107d2bee7f5bfac50006af82df9d611221bf8a63fb5c"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/wyattjoh/ims/releases/download/v1.3.5/ims_v1.3.5_linux_arm64.tar.gz"
-        sha256 "c28db7cf3085635a7d3b894ab6dfe4015f96532d85aabdb2cf12d8f77d7c305c"
+        url "https://github.com/wyattjoh/ims/releases/download/v1.3.6/ims_v1.3.6_linux_arm64.tar.gz"
+        sha256 "d76fa87abc596f1fb3bb320a138886f6d32c6bd5166f195a5f3074cb0911fbfb"
       else
       end
     end
