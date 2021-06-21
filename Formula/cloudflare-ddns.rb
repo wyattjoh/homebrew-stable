392a3769b945559838ea89d53c395eb6c32ad843
goreleaserbot <goreleaser@carlosbecker.com>
2021-06-21 08:55:39 -0600

    Brew formula update for cloudflare-ddns version v1.0.24

--- Formula/cloudflare-ddns.rb
+++ Formula/cloudflare-ddns.rb
@@ -5,20 +5,25 @@
 class CloudflareDdns < Formula
   desc "Cloudflare Dynamic DNS Updater"
   homepage "https://github.com/wyattjoh/cloudflare-ddns"
-  version "1.0.23"
+  version "1.0.24"
   bottle :unneeded
 
-  if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/wyattjoh/cloudflare-ddns/releases/download/v1.0.23/cloudflare-ddns_v1.0.23_darwin_amd64.tar.gz"
-    sha256 "502f3a5d7feb15f430b75e23ab5195b375cd8a82f20963b9bc995ed0916f991b"
+  on_macos do
+    if Hardware::CPU.intel?
+      url "https://github.com/wyattjoh/cloudflare-ddns/releases/download/v1.0.24/cloudflare-ddns_v1.0.24_darwin_amd64.tar.gz"
+      sha256 "3df7bf9855dadf8e521c5f1a3ccbbea35535f87d168753f98c5dc2edd9c3fde4"
+    end
   end
-  if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/wyattjoh/cloudflare-ddns/releases/download/v1.0.23/cloudflare-ddns_v1.0.23_linux_amd64.tar.gz"
-    sha256 "d9fdd724102369795c525ab27d68ca6c359c1e8e56a5044eda7cd588275490de"
-  end
-  if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/wyattjoh/cloudflare-ddns/releases/download/v1.0.23/cloudflare-ddns_v1.0.23_linux_arm64.tar.gz"
-    sha256 "0d04bcca905690a33eae08e32834894bf119dab2b06810cbdfe954752ff988dc"
+
+  on_linux do
+    if Hardware::CPU.intel?
+      url "https://github.com/wyattjoh/cloudflare-ddns/releases/download/v1.0.24/cloudflare-ddns_v1.0.24_linux_amd64.tar.gz"
+      sha256 "bbd075f1ef99f79c45208ef0a6a7cd9037c675151a6461c81c678ac221cb6f6e"
+    end
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/wyattjoh/cloudflare-ddns/releases/download/v1.0.24/cloudflare-ddns_v1.0.24_linux_arm64.tar.gz"
+      sha256 "eaf61e643768148bd5a7c85567eff276a73e04cad29cd0a019e24e24038aeebf"
+    end
   end
 
   def install
