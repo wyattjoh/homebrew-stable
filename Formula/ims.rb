1362c7569d39b99f33a2026fcaadb1820fcfb708
goreleaserbot <goreleaser@carlosbecker.com>
2021-08-09 16:38:25 -0600

    Brew formula update for ims version v1.4.9

--- Formula/ims.rb
+++ Formula/ims.rb
@@ -5,28 +5,28 @@
 class Ims < Formula
   desc "image manipulation service, written in Go"
   homepage "https://github.com/wyattjoh/ims"
-  version "1.4.8"
+  version "1.4.9"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/wyattjoh/ims/releases/download/v1.4.8/ims_v1.4.8_darwin_amd64.tar.gz"
-      sha256 "32153bc99636da18d6b6856069ef15633d7db18a6b4babb9b9436d60a40c1b65"
+      url "https://github.com/wyattjoh/ims/releases/download/v1.4.9/ims_v1.4.9_darwin_amd64.tar.gz"
+      sha256 "96fb1d6431e4a13b0afd23c65e209b7644fa4c1d45be331a60bbb266f384e92e"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/wyattjoh/ims/releases/download/v1.4.8/ims_v1.4.8_darwin_arm64.tar.gz"
-      sha256 "32ccfc798349ffa0f13e1b7396a1e39ab69ff032cef04305f2030021e7a0578f"
+      url "https://github.com/wyattjoh/ims/releases/download/v1.4.9/ims_v1.4.9_darwin_arm64.tar.gz"
+      sha256 "246624cd1dcbdebda79424eae0d8e4f607ebdf28d463674a9929798df990137a"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/wyattjoh/ims/releases/download/v1.4.8/ims_v1.4.8_linux_amd64.tar.gz"
-      sha256 "b0b0d1642e3e6401a22ea611e896a4af9a8988edaadf095ea24b60f44951b52e"
+      url "https://github.com/wyattjoh/ims/releases/download/v1.4.9/ims_v1.4.9_linux_amd64.tar.gz"
+      sha256 "5089204a3b46a0653f83ddc802c3aebfafcb33541842dd2d9298dfe2121bfb18"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/wyattjoh/ims/releases/download/v1.4.8/ims_v1.4.8_linux_arm64.tar.gz"
-      sha256 "7ddb4a7fa45a7cab47f0f37b5bc1f5765eaa2570ef9d592e1f08794cf459f122"
+      url "https://github.com/wyattjoh/ims/releases/download/v1.4.9/ims_v1.4.9_linux_arm64.tar.gz"
+      sha256 "33fea9eb5283b7a26007405bb185ef2505aa64af4c0691d5b8c81f94f444e49d"
     end
   end
 
