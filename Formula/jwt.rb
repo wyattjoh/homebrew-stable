7252880facaea666f03701cf806ba2f21690b66c
goreleaserbot <goreleaser@carlosbecker.com>
2021-11-30 12:15:21 -0700

    Brew formula update for jwt version v0.1.1

--- Formula/jwt.rb
+++ Formula/jwt.rb
@@ -5,20 +5,28 @@
 class Jwt < Formula
   desc "JWT Inspector"
   homepage "https://github.com/wyattjoh/jwt"
-  version "0.1.0"
-  bottle :unneeded
+  version "0.1.1"
 
-  if OS.mac?
-    url "https://github.com/wyattjoh/jwt/releases/download/v0.1.0/jwt_v0.1.0_darwin_amd64.tar.gz"
-    sha256 "de69da930bba7269e6df5f122688571c0e01ffd0d995e5cdf806b913ad3ce43d"
-  end
-  if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/wyattjoh/jwt/releases/download/v0.1.0/jwt_v0.1.0_linux_amd64.tar.gz"
-    sha256 "538aa84a2116ab73f60c9ba37b63f646447aad4d4b9f0b20ad6d34b6489dfbe3"
+  on_macos do
+    if Hardware::CPU.intel?
+      url "https://github.com/wyattjoh/jwt/releases/download/v0.1.1/jwt_v0.1.1_darwin_amd64.tar.gz"
+      sha256 "57b7034e77388de8d9f4251dc1ba57197d4f088a5c4e8298e4f93dc9736a57f1"
+
+      def install
+        bin.install "jwt"
+      end
+    end
   end
 
-  def install
-    bin.install "jwt"
+  on_linux do
+    if Hardware::CPU.intel?
+      url "https://github.com/wyattjoh/jwt/releases/download/v0.1.1/jwt_v0.1.1_linux_amd64.tar.gz"
+      sha256 "d0164fee9023a7890c7df5d42c605c41db0860912ef62c60911914a32d0eaa17"
+
+      def install
+        bin.install "jwt"
+      end
+    end
   end
 
   test do
