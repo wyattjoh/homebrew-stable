56700eafe456a6dff3adb77f33e19d50f9b58d6a
Wyatt Johnson <me@wyattjoh.ca>
2025-09-01 22:11:44 +0000

    vercel-scripts: update to v0.2.0

--- Formula/vercel-scripts.rb
+++ Formula/vercel-scripts.rb
@@ -2,8 +2,8 @@ class VercelScripts < Formula
   desc "Interactive CLI tool for managing Vercel and Next.js development workflows"
   homepage "https://github.com/wyattjoh/vercel-scripts"
   url "https://github.com/wyattjoh/vercel-scripts.git",
-      tag:      "v0.1.19",
-      revision: "87509b66203c25c7b5c19298d33608db4809fbe8"
+      tag:      "v0.2.0",
+      revision: "4cdf6bac98bd3dbcc88b8bb10482321c6fd3e4f3"
   license "MIT"
   head "https://github.com/wyattjoh/vercel-scripts.git", branch: "main"
 
